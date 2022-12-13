fd62869480d5bde28dd3927924ab15cc57b6d24b
TheJPMZ <monzon.jpmz@gmail.com>
2022-12-13 04:10:34 -0600

    ➕ Created the navigationIndex.jsx file

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import * as SplashScreen from 'expo-splash-screen';
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
 import { useFonts } from 'expo-font';
 import { useCallback } from "react";
 
+import NavigationIndex from './navigation/navigationIndex'
 
 SplashScreen.preventAutoHideAsync();
 
@@ -24,34 +26,9 @@ export default function App() {
     }
 
     return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-
-        <TouchableOpacity style={styles.button} onPress={()=>{console.log("A")} }>
-            <Text style={{fontFamily: 'Space Grotesk', fontSize: 26, color: '#C15548'}}>
-                Skip
-            </Text>
-        </TouchableOpacity>
-
-      <StatusBar style="auto" />
-    </View>
+        <SafeAreaProvider>
+            <NavigationIndex/>
+            <StatusBar style="auto" />
+        </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    button:{
-        width: '90%',
-        height: '6vh',
-        backgroundColor: 'white',
-        borderColor: '#C15548',
-        borderWidth: 2,
-        alignItems: "center",
-        justifyContent: "center"
-    }
-});
---
+++ navigation/navigationIndex.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import InterestsScreen from "../screens/InterestsScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function NavigationIndex() {
+    return (
+        <NavigationContainer >
+            <Root/>
+        </NavigationContainer>
+    )
+}
+
+function Root(){
+    return (
+        <Stack.Navigator screenOptions={{headerShown:false}}>
+            <Stack.Screen name={"Interests"} component={InterestsScreen} />
+        </Stack.Navigator>
+    )
+}
